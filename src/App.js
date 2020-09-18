94a8ea7aee142034cef67bb6c9f08a0427efb6a0
Darren <darrenzhang200027@gmail.com>
2020-09-18 12:01:07 -0400

    login + reg

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { Route } from 'react-router-dom';
+import Register from './pages/register/register.page';
+import Landing from './pages/landing/landing.page';
+import { Provider } from 'react-redux';
+import store from './redux/redux';
+
+
+function App() {
+  return (
+    <Provider store={store}>
+
+    <div>
+      <Header />
+      <Route exact path='/' component={Landing} />
+      <Route exact path='/register' component={Register} />
+      <Bottom />
+    </div>
+    </Provider>
+
+  );
+}
+
+export default App;
