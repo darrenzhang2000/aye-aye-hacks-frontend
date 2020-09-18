fcb6ede168f9a8b3c7eef1bea2a09e093be34257
Darren <darrenzhang200027@gmail.com>
2020-09-18 14:33:04 -0400

    settings page view

--- src/pages/settings/settings.js
+++ src/pages/settings/settings.js
@@ -1,25 +1,74 @@
 import React from 'react'
-import {TextField} from '@material-ui/core'
+import { FormControlLabel, Switch, TextField, Typography } from '@material-ui/core'
+import "./settings.css"
 
 const Settings = (props) => {
     return <div>
 
+        <Typography>Setting your goal</Typography>
+        <TextField
+            required
+            id="filled-required"
+            label="Type your fitness goal here"
+            defaultValue=""
+            variant="filled"
+        />
+
+
+        <FormControlLabel
+            control={
+                <Switch
+                    checked={true}
+                    onChange={() => { }}
+                    name="switch"
+                />
+            }
+            label="Share your goal and progress"
+        />
+
+
+
+
+        <Typography>Account Information</Typography>
 
         <form>
-            <TextField
-                required
-                id="filled-required"
-                label="Required"
-                defaultValue="Name"
-                variant="filled"
-            />
-            <TextField
-                required
-                id="filled-required"
-                label="Email"
-                defaultValue="Email"
-                variant="filled"
-            />
+            <div className="setting-form">
+                <TextField
+                    required
+                    id="filled-required"
+                    label="Required"
+                    defaultValue=""
+                    variant="filled"
+                />
+                <TextField
+                    required
+                    id="filled-required"
+                    label="Email"
+                    defaultValue=""
+                    variant="filled"
+                />
+                <TextField
+                    required
+                    id="filled-required"
+                    label="Date of birth"
+                    defaultValue=""
+                    variant="filled"
+                />
+                <TextField
+                    required
+                    id="filled-required"
+                    label="Gender"
+                    defaultValue=""
+                    variant="filled"
+                />
+                <TextField
+                    required
+                    id="filled-required"
+                    label="Country"
+                    defaultValue=""
+                    variant="filled"
+                />
+            </div>
         </form>
     </div>
 }
