d5ebd818f3de6aff4777202c72399aa6ebd896fd
probeta <33081197+ProBeta1@users.noreply.github.com>
2020-09-19 21:00:38 +0530

    progress page , grid consuming too much space

--- src/pages/progress/progress.js
+++ src/pages/progress/progress.js
@@ -1,36 +1,45 @@
 import { Typography, Grid } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
 import "./progress.css"
+import { VictoryPie } from 'victory';
+
 
 const Progress = (props) => {
-    return <div>
-        <Typography>Goals</Typography>
+
+    const [data, setData] = useState([
+        { x: "Cats", y: 35 },
+        { x: "Dogs", y: 40 },
+        { x: "Birds", y: 55 }
+    ]);
+
+    return <div className="background">
+        <p className="goal-title">Goals</p>
         <Grid container className="row">
             <Grid sm={0} md={3}></Grid>
-            <Grid md={6}>Workout everyday</Grid>
-            <Grid md={3}>Until Sep 31</Grid>
+            <Grid md={6}><p className="goal-text">Workout everyday</p></Grid>
+            <Grid md={3}><p className="goal-text">Until Sep 31</p> </Grid>
         </Grid>
 
 
-        <Typography>Achievement</Typography>
+        <p className="goal-title">Achievement</p>
         <Grid container className="row">
             <Grid sm={0} md={3}></Grid>
 
             <Grid container md={6} className="row">
 
                 <div className="col">
-                    <Typography>10</Typography>
-                    <Typography>Days</Typography>
+                    <p className="achieve-text">10</p>
+                    <p>Days</p>
                 </div>
 
                 <div className="col">
-                    <Typography>200</Typography>
-                    <Typography>Minutes</Typography>
+                    <p className="achieve-text">200</p>
+                    <p>Minutes</p>
                 </div>
 
                 <div className="col">
-                    <Typography>15</Typography>
-                    <Typography>Sessions</Typography>
+                    <p className="achieve-text">15</p>
+                    <p>Sessions</p>
                 </div>
 
             </Grid>
@@ -39,13 +48,21 @@ const Progress = (props) => {
         </Grid>
 
 
-        <Typography>Categories breakdown</Typography>
-        <Typography>### PLACEHOLDER FOR PIE CHART ###</Typography>
+        <p className="goal-title">Categories breakdown</p>
+
+        <Grid container className="row">
+            <Grid sm={0} md={3} justify="center">
+                <VictoryPie
+                    innerRadius={100}
+                    data={data}
+                />
+            </Grid>
+        </Grid>
 
 
 
-        <Typography>Progress</Typography>
-        <Typography>### PLACEHOLDER FOR CALENDAR ###</Typography>
+        <p className="goal-title">Progress</p>
+        <p>### PLACEHOLDER FOR CALENDAR ###</p>
     </div>
 }
 
